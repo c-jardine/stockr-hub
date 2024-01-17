4d9d39acf00bde5699dff5e08fd34171bf28280a
Chris Jardine <dev@keplux.com>
2024-01-17 15:09:05 -0500

    Adjust button styles.

--- src/styles/theme/components/button.ts
+++ src/styles/theme/components/button.ts
@@ -1,15 +1,17 @@
 import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
 
 const solid = defineStyle({
-  fontWeight: '500',
+  fontWeight: '600',
   fontSize: 'sm',
+  letterSpacing: 'wide',
 });
 
 const outline = defineStyle({
   fontWeight: '700',
   fontSize: 'sm',
+  letterSpacing: 'wide',
   borderColor: 'slate.200',
-  color: 'slate.700'
+  color: 'slate.700',
 });
 
 export const buttonTheme = defineStyleConfig({
