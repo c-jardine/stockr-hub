4ca47ff918888307beb3c343d826eadce37bebcf
Chris Jardine <dev@keplux.com>
2024-01-17 23:18:05 -0500

    Refactor the MaterialsUsed component.

--- src/features/products/components/MaterialsUsed.tsx
+++ src/features/products/components/MaterialsUsed.tsx
@@ -11,62 +11,77 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 
-export default function MaterialsUsed(props: ProductGetAllOutputSingle) {
-  return (
-    <>
-      {props.materials.map(({ material, ...rest }) => (
-        <Box key={material.id} fontSize='sm'>
-          <UpdateMaterialDrawer {...material} />
-          <HStack
-            divider={<StackDivider border='none'>&bull;</StackDivider>}
-            alignItems='flex-start'
-          >
-            <Text color='slate.500' letterSpacing='wide'>
-              {round(Number(rest.quantity) / props.batchSize)}{' '}
-              {getStockUnitTextAbbrev(
-                round(Number(rest.quantity) / props.batchSize),
-                material.stockLevel.stockUnit
-              )}{' '}
-              used
-            </Text>
-            <Tooltip
-              label={`Enough for ${Math.floor(
-                Number(material.stockLevel.stock) /
-                  (Number(rest.quantity) / props.batchSize)
-              )} ${getStockUnitTextAbbrev(
-                Math.floor(
-                  Number(material.stockLevel.stock) /
-                    (Number(rest.quantity) / props.batchSize)
-                ),
-                props.stockLevel.stockUnit
-              )}`}
-            >
-              <CustomTooltip color='slate.500' letterSpacing='wide'>
-                {Number(material.stockLevel.stock)}{' '}
-                {getStockUnitTextAbbrev(
-                  Number(material.stockLevel.stock),
-                  material.stockLevel.stockUnit
-                )}{' '}
-                available
-              </CustomTooltip>
-            </Tooltip>
-            <Text color='slate.500' letterSpacing='wide'>
-              {round(
-                (Number(material.costPerUnit) * Number(rest.quantity)) /
-                  props.batchSize
-              )}{' '}
-              per unit
-            </Text>
-          </HStack>
-        </Box>
-      ))}
-    </>
-  );
+/**
+ * A component that shows information about the materials used for the given
+ * product.
+ */
+export default function MaterialsUsed({
+  materials,
+  batchSize,
+  stockLevel: { stockUnit },
+}: ProductGetAllOutputSingle) {
+  // Render helper function for the component.
+  function renderMaterialInfo({
+    material,
+    quantity,
+  }: ProductGetAllOutputSingle['materials'][0]) {
+    // Data for the amount of stock used.
+    const stockUsed = round(Number(quantity) / batchSize);
+    const stockUsedUnit = getStockUnitTextAbbrev(
+      stockUsed,
+      material.stockLevel.stockUnit
+    );
+
+    // Data for how many of this product can be created with the material's
+    // stock.
+    const enoughFor = Math.floor(
+      Number(material.stockLevel.stock) / (Number(quantity) / batchSize)
+    );
+    const enoughForUnit = getStockUnitTextAbbrev(enoughFor, stockUnit);
+
+    // Data for the material's stock level.
+    const stockLevel = Number(material.stockLevel.stock);
+    const stockLevelUnit = getStockUnitTextAbbrev(
+      stockLevel,
+      material.stockLevel.stockUnit
+    );
+
+    // Data for the material's cost per individual unit.
+    const costPerUnit = round(
+      (Number(material.costPerUnit) * Number(quantity)) / batchSize
+    );
+
+    return (
+      <Box key={material.id} fontSize='sm'>
+        <UpdateMaterialDrawer {...material} />
+        <HStack
+          divider={<StackDivider border='none'>&bull;</StackDivider>}
+          alignItems='flex-start'
+        >
+          <Text color='slate.500' letterSpacing='wide'>
+            {stockUsed} {stockUsedUnit} used
+          </Text>
+          <Tooltip label={`Enough for ${enoughFor} ${enoughForUnit}`}>
+            <EnoughForTooltip>
+              {stockLevel} {stockLevelUnit} available
+            </EnoughForTooltip>
+          </Tooltip>
+          <Text color='slate.500' letterSpacing='wide'>
+            ${costPerUnit} per unit
+          </Text>
+        </HStack>
+      </Box>
+    );
+  }
+
+  // The rendered component.
+  return <>{materials.map(renderMaterialInfo)}</>;
 }
 
-const CustomTooltip = React.forwardRef<HTMLDivElement, TextProps>(
-  ({ children, ...rest }, ref) => (
-    <Text ref={ref} {...rest}>
+// A custom tooltip.
+const EnoughForTooltip = React.forwardRef<HTMLDivElement, TextProps>(
+  ({ children }, ref) => (
+    <Text ref={ref} color='slate.500' letterSpacing='wide'>
       {children}
     </Text>
   )
