19f1106e420283176a8d0b46f722725ee82a2301
Chris Jardine <dev@keplux.com>
2024-01-25 02:42:25 -0500

    Fix bug not allowing material categories to be created.

--- src/features/material/components/MaterialCategoriesInput/hooks/useCreateMaterialCategory.ts
+++ src/features/material/components/MaterialCategoriesInput/hooks/useCreateMaterialCategory.ts
@@ -1,15 +1,15 @@
-import { type ProductCreate, type ProductUpdate } from '@/types';
+import { type MaterialCreate, type MaterialUpdate } from '@/types';
 import { api } from '@/utils/api';
 import { useToast } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 
 export default function useCreateMaterialCategory() {
-  const form = useFormContext<ProductCreate | ProductUpdate>();
+  const form = useFormContext<MaterialCreate | MaterialUpdate>();
 
   const toast = useToast();
 
   const utils = api.useUtils();
-  const query = api.product.createCategory.useMutation({
+  const query = api.material.createCategory.useMutation({
     onError: (data) => {
       toast({
         title: 'Error',
@@ -23,7 +23,7 @@ export default function useCreateMaterialCategory() {
         description: 'Successfully created category',
         status: 'success',
       });
-      await utils.product.getAllCategories.invalidate();
+      await utils.material.getAllCategories.invalidate();
       const prev = form.getValues('categoryIds');
       if (prev) {
         form.setValue('categoryIds', [...prev, data.id]);
