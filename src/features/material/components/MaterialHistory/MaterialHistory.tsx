afa50dd07562a1ba9c9fed511bb94e4014522f5e
Chris Jardine <chris@keplux.com>
2024-03-19 18:50:03 -0400

    SH-41 Extract material and product history tabs to separate component.

--- src/features/material/components/MaterialHistory/MaterialHistory.tsx
+++ src/features/material/components/MaterialHistory/MaterialHistory.tsx
@@ -1,12 +1,5 @@
+import { InventoryHistoryTabs } from "@/components/InventoryHistoryTabs";
 import { StockGraph } from "@/components/StockGraph";
-import {
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-} from "@chakra-ui/react";
 import { useMaterial } from "../../hooks";
 import MaterialHistoryItem from "./MaterialHistoryItem";
 import { useMaterialHistory } from "./hooks";
@@ -17,25 +10,15 @@ export default function MaterialHistory() {
   const { query } = useMaterialHistory();
 
   return (
-    <Stack>
-      <Tabs>
-        <TabList>
-          <Tab fontSize={{ base: "xs", sm: "md" }}>History</Tab>
-          <Tab fontSize={{ base: "xs", sm: "md" }}>Visualization</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <MaterialHistoryItem
-              history={query.data!}
-              stockUnit={material.stockLevel.stockUnit}
-              createdAt={material.createdAt}
-            />
-          </TabPanel>
-          <TabPanel>
-            <StockGraph {...material} />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Stack>
+    <InventoryHistoryTabs
+      historyTab={
+        <MaterialHistoryItem
+          history={query.data!}
+          stockUnit={material.stockLevel.stockUnit}
+          createdAt={material.createdAt}
+        />
+      }
+      visualizationTab={<StockGraph {...material} />}
+    />
   );
 }
--- src/features/products/components/ProductHistory/ProductHistory.tsx
+++ src/features/products/components/ProductHistory/ProductHistory.tsx
@@ -1,12 +1,5 @@
+import { InventoryHistoryTabs } from "@/components/InventoryHistoryTabs";
 import { StockGraph } from "@/components/StockGraph";
-import {
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-} from "@chakra-ui/react";
 import useProduct from "../../hooks/useProduct";
 import ProductHistoryItem from "./HistoryItem";
 import { useProductHistory } from "./hooks";
@@ -17,25 +10,15 @@ export default function ProductHistory() {
   const { query } = useProductHistory();
 
   return (
-    <Stack>
-      <Tabs>
-        <TabList>
-          <Tab fontSize={{ base: "xs", sm: "md" }}>History</Tab>
-          <Tab fontSize={{ base: "xs", sm: "md" }}>Visualization</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <ProductHistoryItem
-              history={query.data!}
-              stockUnit={product.stockLevel.stockUnit}
-              createdAt={product.createdAt}
-            />
-          </TabPanel>
-          <TabPanel>
-            <StockGraph {...product} />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Stack>
+    <InventoryHistoryTabs
+      historyTab={
+        <ProductHistoryItem
+          history={query.data!}
+          stockUnit={product.stockLevel.stockUnit}
+          createdAt={product.createdAt}
+        />
+      }
+      visualizationTab={<StockGraph {...product} />}
+    />
   );
 }
